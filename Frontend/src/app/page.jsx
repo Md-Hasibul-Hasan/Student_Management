a8f9219aefa9cf241593c014b46d0be2b789abb1
Md Hasibul Hasan <hasib.hasan.th@gmail.com>
2026-06-14 04:58:13 +0600

    fix again

--- Frontend/src/app/page.jsx
+++ Frontend/src/app/page.jsx
@@ -1,11 +1,5 @@
-import React from 'react';
+import { redirect } from "next/navigation";
 
-const page = () => {
-  return (
-    <div>
-      <h1>Home Page</h1>
-    </div>
-  );
-};
-
-export default page;
+export default function Page() {
+  redirect("/Student");
+}
